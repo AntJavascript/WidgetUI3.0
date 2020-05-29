b66b337ea76e1b9363d24d659e550dc4a81db763
石良钰 <337265223@qq.com>
2020-05-29 21:27:19 +0800

    新增 Card 卡片组件

---
+++ Card.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="wt-card" :style="{borderRadius: radius, padding: padding, margin: margin}">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    radius: {
+      type: String,
+      default: () => {
+        return '0.3rem'
+      }
+    },
+    padding: {
+      type: String,
+      default: () => {
+        return '0.4rem'
+      }
+    },
+    margin: {
+      type: String,
+      default: () => {
+        return '0.4rem'
+      }
+    },
+  }
+}
+</script>
+<style lang="less" scoped>
+.wt-card {
+  margin: 0.4rem;
+  background: #fff;
+  overflow: hidden;
+  box-shadow: 0 0 0.2rem #ccc;
+  border-bottom: 0.1rem solid #ccc;
+}
+</style>
