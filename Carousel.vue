d8bedbb7db9f0f2c680deaabddab698c684ec686
XiaoShi <337265223@qq.com>
2020-05-29 16:12:54 +0800

    Update Carousel.vue

--- Carousel.vue
+++ Carousel.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="wt-carousel" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd">
-    <div class="wrapper" :style="{transform: 'translate3d('+ state.distance +'px, 0px, 0px)'}">
+    <div
+    v-if="options.direction == 'horizontal'"
+    :class="options.direction + ' wrapper'"
+    :style="{transform: 'translate3d('+ state.distance +'px, 0px, 0px)'}">
       <slot></slot>
     </div>
-    <div class="carousel-pagination" v-if="options.pagination">
-      <p v-for="(item, index) in state.itemCount" :key="index" :class="{'active':index === state.currentIndex}"></p>
+    <div
+    v-else-if="options.direction == 'vertical'"
+    :class="options.direction + ' wrapper'"
+    :style="{transform: 'translate3d(0px, '+ state.distance +'px, 0px)', height:(options.height || state.itemWidth) + 'px'}">
+      <slot></slot>
+    </div>
+    <div :class="options.direction + ' carousel-pagination'" v-if="options.pagination">
+      <p v-for="(item, index) in state.itemCount" 
+      :key="index" 
+      :class="{'active':index === state.currentIndex}"
+      :style="{background:index === state.currentIndex ? options.paginationColor : ''}"
+      ></p>
     </div>
   </div>
 </template>
@@ -17,10 +30,12 @@ export default {
       type: Object,
       default: () => {
         return {
-          loop: false,
-          pagination: true,
-          auto: false,
-          time: 3000
+          loop: false, // 循环
+          pagination: true, // 指示器
+          auto: false, // 自动
+          time: 3000, // 播放间隔
+          paginationColor: '#fff', // 当前指示器颜色
+          direction: 'horizontal' // 水平方向 (vertical 垂直方向)
         };
       }
     }
@@ -33,7 +48,8 @@ export default {
       currentIndex: 0, // 当前处于第几个
       distance: 0, // 滑动距离
       itemCount: 0, // 有多少item
-      thisDistance: 0, // 本次滑动距离
+      thisDistanceX: 0, // 本次滑动距离X
+      thisDistanceY: 0, // 本次滑动距离Y
       start: { X: 0, Y: 0 }, // 触摸坐标
       move: { X: 0, Y: 0 }, // 移动坐标
       slots: [], // 父组件的slot
@@ -51,15 +67,29 @@ export default {
         if (state.currentIndex > state.itemCount) {
           state.currentIndex = 0;
           firstChild.style.transitionDuration = "0ms";
-          state.distance = -state.itemWidth;
+          if (props.options.direction === 'horizontal') {
+            state.distance = -state.itemWidth;
+          } else if (props.options.direction === 'vertical') {
+            state.distance = -props.options.height;
+          }
         } else {
-          state.distance = -((state.currentIndex + 1) * state.itemWidth);
+         if (props.options.direction === 'horizontal') {
+           state.distance = -((state.currentIndex + 1) * state.itemWidth);
+          } else if (props.options.direction === 'vertical') {
+            state.distance = -((state.currentIndex + 1) * props.options.height);
+          }
+          
           if (state.currentIndex === state.itemCount) {
             state.currentIndex = 0;
             // 400ms之后滚动到第一张，并且过渡时间为0, 时间一定要设置大于300ms，否则会有闪屏问题
             setTimeout(() => {
               firstChild.style.transitionDuration = "0ms";
-              state.distance = -state.itemWidth;
+              if (props.options.direction === 'horizontal') {
+               state.distance = -state.itemWidth;
+              } else if (props.options.direction === 'vertical') {
+                state.distance = -props.options.height;
+              }
+              
             }, 400);
           }
         }
@@ -67,11 +97,14 @@ export default {
         // 不循环轮播
         if (state.currentIndex === state.itemCount - 1) {
           state.currentIndex = 0;
-          state.distance = -(state.currentIndex * state.itemWidth);
         } else {
           state.currentIndex++;
-          state.distance = -(state.currentIndex * state.itemWidth);
         }
+         if (props.options.direction === 'horizontal') {
+          state.distance = -(state.currentIndex * state.itemWidth);
+          } else if (props.options.direction === 'vertical') {
+           state.distance = -(state.currentIndex * props.options.height);
+          }
       }
       ctx.$emit("swiper", state.currentIndex);
     };
@@ -80,12 +113,16 @@ export default {
       const len = ctx.$el.children[0].children.length;
       // 处理每个item的宽度
       for (let i = 0; i < len; i++) {
-        if (ctx.$el.children[0].children[i].className.indexOf('carousel-item') > -1) {
+        // if (ctx.$el.children[0].children[i].className.indexOf('carousel-item') > -1) {
           const elm = ctx.$el.children[0].children[i];
           elm.style.width = state.itemWidth + "px"; // 设置每个item的宽度
           state.slots.push(ctx.$el.children[0].children[i]);
           state.itemCount++;
-        }
+          // 垂直方向，需要设置高度
+          if (props.options.direction === 'vertical' && (props.options.height || state.itemWidth)) {
+             elm.style.height = (props.options.height || state.itemWidth) + "px";
+          }
+        // }
       }
       // 需要重复轮播
       if (props.options.loop) {
@@ -97,7 +134,12 @@ export default {
         const last = state.slots[state.slots.length - 1].cloneNode(true);
         firstChild.insertBefore(last, firstChild.childNodes[0]);
         firstChild.style.transitionDuration = "0ms";
-        state.distance = -state.itemWidth;
+        
+        if (props.options.direction === 'horizontal') {
+          state.distance = -state.itemWidth;
+        } else if (props.options.direction === 'vertical') {
+         state.distance = -props.options.height;
+        }
       }
       // 是否自动轮播
       if (props.options.auto) {
@@ -125,35 +167,45 @@ export default {
       ) {
         event.preventDefault();
         event.stopPropagation();
-      } else {
+      } else if(props.options.direction === 'horizontal') {
         // 如果是竖着滑动，则 return
         return;
       }
-      // 本次滑动距离
-      const thisDistance = state.move.X - state.start.X;
-      state.thisDistance = thisDistance;
+      // 本次滑动距离X
+      const thisDistanceX = state.move.X - state.start.X;
+      state.thisDistanceX = thisDistanceX;
+      // 本次滑动距离X
+      const thisDistanceY = state.move.Y - state.start.Y;
+      state.thisDistanceY = thisDistanceY;
       // 如果是循环轮播，则是currentIndex + 1
-      let resistance = 1;
+      let resistance = 1; // 滑动阻力
       if (
-        (thisDistance > 0 && state.currentIndex === 0) ||
-        (thisDistance < 0 && state.currentIndex === state.itemCount - 1)
+        (thisDistanceX > 0 && state.currentIndex === 0) ||
+        (thisDistanceX < 0 && state.currentIndex === state.itemCount - 1)
       ) {
         resistance = 0.2;
       }
+      // distance 滑动距离
+      let index = state.currentIndex;
       if (props.options.loop) {
-        state.distance =
-          -((state.currentIndex + 1) * state.itemWidth) +
-          thisDistance * resistance;
-      } else {
-        state.distance =
-          -(state.currentIndex * state.itemWidth) + thisDistance * resistance;
+        index = state.currentIndex + 1;
+      }
+      if (props.options.direction === 'horizontal') {
+        state.distance = -(index * state.itemWidth) + thisDistanceX * resistance;
+      } else if (props.options.direction === 'vertical') {
+       state.distance = -(index * props.options.height) + thisDistanceY * resistance;
       }
     };
+    // 是否循环
     const isLoop = () => {
+      let index = state.currentIndex;
       if (props.options.loop) {
-        state.distance = -(state.itemWidth * (state.currentIndex + 1));
-      } else {
-        state.distance = -(state.itemWidth * state.currentIndex);
+        index = state.currentIndex + 1;
+      }
+      if (props.options.direction === 'horizontal') {
+        state.distance = -(state.itemWidth * index);
+      } else if (props.options.direction === 'vertical') {
+       state.distance = -(props.options.height * index);
       }
     };
     const touchEnd = () => {
@@ -165,18 +217,19 @@ export default {
       // 如果是竖着滑动，则 return
       if (
         Math.abs(state.move.Y - state.start.Y) >
-        Math.abs(state.move.X - state.start.X)
+        Math.abs(state.move.X - state.start.X) &&
+        props.options.direction === 'horizontal'
       ) {
         return;
       }
-      // 如果state.move.X === 0说明没有移动
+      // 如果state.status === ""说明没有移动
       if (state.status === "") {
         return;
       }
       firstChild.style.transitionDuration = "300ms";
-      // 往左滑
-      if (state.thisDistance < 0) {
-        if (state.thisDistance <= -(state.itemWidth / 3)) {
+      // 往左滑 || 往下滑
+      if (state.thisDistanceX < 0 || state.thisDistanceY < 0) {
+        if (state.thisDistanceX <= -(state.itemWidth / 3) || state.thisDistanceY <= -(props.options.height /3)) {
           // 当前index 必须小于item数量减1
           if (state.currentIndex < state.itemCount - 1) {
             state.currentIndex++;
@@ -186,41 +239,44 @@ export default {
             }
           }
           ctx.$emit("swiper", state.currentIndex);
-          isLoop();
-        } else {
-          isLoop();
         }
+        isLoop();
         // 如果是无限循环，则需要把索引变成0
         if (props.options.loop) {
           if (state.itemCount === state.currentIndex) {
             state.currentIndex = 0;
             setTimeout(() => {
               firstChild.style.transitionDuration = "0s";
-              state.distance = -state.itemWidth;
+               if (props.options.direction === 'horizontal') {
+                  state.distance = -state.itemWidth;
+                } else if (props.options.direction === 'vertical') {
+                 state.distance = -props.options.height;
+                }
             }, 300);
           }
         }
       } else {
-        // 往右滑
-        if (state.thisDistance >= state.itemWidth / 3) {
+        // 往右滑 || 往上滑
+        if (state.thisDistance >= state.itemWidth / 3 || state.thisDistanceY <= props.options.height / 3) {
           // 当前index 必须小于item数量减1
           state.currentIndex--;
           if (!props.options.loop && state.currentIndex <= 0) {
             state.currentIndex = 0;
           }
           ctx.$emit("swiper", state.currentIndex);
-          isLoop();
-        } else {
-          isLoop();
         }
+        isLoop();
         // 如果是无限循环，则需要把索引变成最后一个
         if (props.options.loop) {
           if (state.currentIndex < 0) {
-            state.currentIndex = state.itemCount - 1;
-            ctx.$emit("swiper", state.currentIndex);
+            ctx.$emit("swiper", state.currentIndex - 1);
             setTimeout(() => {
               firstChild.style.transitionDuration = "0s";
-              state.distance = -(state.itemWidth * (state.currentIndex + 1));
+              if (props.options.direction === 'horizontal') {
+                 state.distance = -(state.itemWidth * state.currentIndex);
+              } else if (props.options.direction === 'vertical') {
+                state.distance = -(state.itemWidth * state.currentIndex);
+              }
             }, 300);
           }
         }
@@ -251,6 +307,10 @@ export default {
     transition-duration: 300ms;
     transition-timing-function: ease-out;
     transition-property: transform;
+    &.vertical {
+      display: flex;
+      flex-direction: column;
+    }
     .carousel-item {
       text-align: center;
       font-size: 0.8rem;
@@ -270,6 +330,13 @@ export default {
     position: absolute;
     bottom: 0.3rem;
     width: 100%;
+    &.vertical {
+      right: 0;
+      top: 0;
+      width: 30px;
+      flex-direction: column;
+      justify-content: center;
+    }
     p {
       margin: 0.2rem;
       height: 0.3rem;
