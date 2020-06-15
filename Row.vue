0f82ae86f16dd9b8dda80285c6bb0c7aa09ffa97
XiaoShi <337265223@qq.com>
2020-06-15 15:21:21 +0800

    Create Row.vue

---
+++ Row.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class='wt-row'>
+        <slot></slot>
+    </div>
+</template>
+<script>
+</script>
+<style lang='less' rel='stylesheet/less' scoped>
+ .wt-row {
+    display: flex;
+    align-items: center;
+    height: 100%;
+ }
+</style>
