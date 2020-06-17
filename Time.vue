8ec424ac0f8a5491ce1bb35e154f9c4320e7861e
XiaoShi <337265223@qq.com>
2020-06-17 16:47:08 +0800

    Create Time.vue

---
+++ Time.vue
@@ -0,0 +1,37 @@
+<template>
+  <span>{{showTime}}</span>
+</template>
+<script>
+import { computed } from 'vue';
+import {utils} from './utils';
+export default {
+  props: {
+    time: {
+      type: String || Number,
+      default: () => {
+        return "";
+      }
+    },
+    type: {
+      type: String,
+      default: () => {
+        return "";
+      }
+    },
+    format: {
+      type: String,
+      default: () => {
+        return "";
+      }
+    }
+  },
+  setup (props) {
+    const showTime = computed(()=> {
+      return utils.getcurrentDate(props.time, props.type || "date");
+    })
+    return {
+      showTime
+    };
+  }
+};
+</script>
