8e3d7252b4dbc48afe2864adc989f97fd7220b25
XiaoShi <337265223@qq.com>
2020-06-18 12:14:44 +0800

    Create Alert.js

---
+++ Alert.js
@@ -0,0 +1,63 @@
+import {wtDom} from './utils';
+/*
+ * @params options {Object} 必须
+*/
+function Alert (params) {
+  var defaults = {
+    title: '',
+    content: '',
+    btns: [],
+    zIndex: 100,
+    // type: "android"
+  };
+  var options = Object.assign(defaults, params);
+  var Alert = wtDom.create('div');
+  Alert.id = 'wt-Alert';
+  // 如果title不为空
+  if (options.title !== '') {
+    var title = wtDom.create();
+    title.className = 'title';
+    title.innerText = options.title;
+    Alert.appendChild(title);
+  }
+  // 显示内容
+  var content = wtDom.create();
+  content.className = 'content';
+  content.innerText = options.content;
+  Alert.appendChild(content);
+  // 显示btn
+  var btns = wtDom.create();
+  btns.className = 'wt-Alert-btns';
+  options.btns.forEach((item, index) => {
+    var btn = wtDom.create();
+    btn.className = 'wt-Alert-btn';
+    btn.innerText = item.text;
+    btn.addEventListener('click', function () {
+      wtDom.del(mask);
+      wtDom.del(Alert);
+      typeof item.handle == 'function' && item.handle(index);
+    });
+    btns.appendChild(btn);
+  });
+  Alert.appendChild(btns);
+  Alert.style.zIndex = options.zIndex + 1;
+  // 添加Alert
+  document.body.appendChild(Alert);
+  // 遮罩层
+  var mask = wtDom.create();
+  mask.className = 'wt-alert-mask';
+  mask.style.zIndex = options.zIndex;
+  mask.addEventListener('click', function () {
+    if (options.btns.length === 0 ) {
+      wtDom.del(mask);
+      wtDom.del(Alert);
+    }
+  });
+  // 阻止滑动
+   mask.addEventListener('touchmove', function () {
+    event.preventDefault()
+  });
+  // 添加遮罩层
+  document.body.appendChild(mask);
+}
+export default Alert;
